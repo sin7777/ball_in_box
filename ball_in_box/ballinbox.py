6efd063922000f42da2fa3a455fce433938bdcff
sin7777 <jasminelu7777@gmail.com>
2018-06-15 21:36:12 +0800

    更新

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -4,29 +4,33 @@ from .validate import validate
 
 __all__ = ['ball_in_box']
 
+
 def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
     """
     m is the number circles.
     n is the list of coordinates of tiny blocks.
-    
+
     This returns a list of tuple, composed of x,y of the circle and r of the circle.
     """
 
-    # The following is an example implementation.
     circles = []
+
     for circle_index in range(m):
-
-        x = random.random()*2 - 1
-        y = random.random()*2 - 1
-        r = random.random()*0.1
-
-        circles.append((x, y, r))
-        while not validate(circles, blockers):
-            x = random.random()*2 - 1
-            y = random.random()*2 - 1
-            r = random.random()*0.1
-            circles[circle_index] = (x, y, r)
-
+        tmp1 = 0
+        circles.append((0, 0, 0))
+        for i in range(200):
+            for j in range(200):
+                x = -1 + 0.01 * i
+                y = -1 + 0.01 * j
+                for k in range(200):
+                    r = 0.01 * k
+                    circles[circle_index] = (x, y, r)
+                    if not validate(circles, blockers):
+                        break
+                if r - 0.01 > tmp1:
+                    tmp1 = r - 0.01
+                    tmp_circle = (x, y, tmp1)
+        circles[circle_index] = tmp_circle
         circle_index += 1
-    
+
     return circles
